4f3d13c1a6c106e2248c9a5946dba25f1b020260
hello <hello@LAPTOP-QREMTL3O>
2022-04-21 21:11:29 +0900

    내 힘으로 만들어 본 두번째코드

---
+++ Homework/src/StudentMain.java
@@ -0,0 +1,24 @@
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+      Student s1 = new Student("5.1","박길동","76점","90점","100점");
+      
+       
+      
+      
+      
+      
+      
+      
+       
+       s1.getInfo();
+       
+       
+       }
+
+
+}
+
+		
+	
--- Pratice/src/Main.java
+++ Pratice/src/Main.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] arg) {
 		System.out.println("Hello, World");
 		System.out.println("Bye, World");
-		System.out.println("안녕");
-		System.out.println("안녕 코시들 만나서 반가워");
-
+		
+		System.out.println("                   ");
+		
+		System.out.println("콱시"+"♥");
+		
+		System.out.println("-------------------------------------------------------");
+		
+		
+		System.out.println("강철부대원픽 고르기");
+		
+		
+		
+		System.out.println("-------------------------------------------------------");
+		
+		
+	
+		
+		
+		
+System.out.println("-------------------------------------------------------");
+		
+		
+		System.out.println("1.권호제 2.윤종진 3.구민철 4.명단단");
+		
+		
+		
+		System.out.println("-------------------------------------------------------");
+		
+		
+		
+		
+		
 	}
-}
+	}
