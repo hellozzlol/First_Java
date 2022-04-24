4db57d41143d05a96fe3e5bc2e2fad87729fe55c
hello <hello@LAPTOP-QREMTL3O>
2022-04-24 22:56:34 +0900

    태어나서 두번째로 만든 시험평균계산기

--- Homework/src/StudentMain.java
+++ Homework/src/StudentMain.java
@@ -2,16 +2,9 @@
 public class StudentMain {
 
 	public static void main(String[] args) {
-      Student s1 = new Student("5.1","박길동","76점","90점","100점");
+      Student s1 = new Student("1602184-1","박길동","76점","90점","100점");
       
-       
-      
-      
-      
-      
-      
-      
-       
+     
        s1.getInfo();
        
        
--- Pratice/src/Main.java
+++ Pratice/src/Main.java
@@ -3,38 +3,87 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] arg) {
-		System.out.println("Hello, World");
-		System.out.println("Bye, World");
+		//System.out.println("Hello, World");
+		//System.out.println("Bye, World");
 		
-		System.out.println("                   ");
+		//System.out.println("                   ");
 		
-		System.out.println("콱시"+"♥");
+		//System.out.println("콱시"+"♥");
+		//System.out.println("-------------------------------------------------------");
 		
-		System.out.println("-------------------------------------------------------");
+		//System.out.println("1.학생이름 2.학번 3.국어 4.영어 5.수학 6.합계 7.평균 8.종료");
+		
+		//System.out.println("------------------------------입력하세요->>>>>>-----------------");
 		
 		
-		System.out.println("강철부대원픽 고르기");
+		//System.out.println("강철부대원픽 고르기");
 		
 		
 		
-		System.out.println("-------------------------------------------------------");
+		//System.out.println("-------------------------------------------------------");
 		
 		
 	
 		
 		
 		
-System.out.println("-------------------------------------------------------");
+//System.out.println("-----------------------------------------------------");
 		
 		
-		System.out.println("1.권호제 2.윤종진 3.구민철 4.명단단");
-		
-		
-		
-		System.out.println("-------------------------------------------------------");
+		//System.out.println("1.권호제 2.윤종진 3.구민철 4.명단단");
 		
 		
 		
+		/*System.out.println("-------------------------------------------------------");
+		
+		
+		
+		
+		/*String str1 = 20 + 2+"8";
+		System.out.println("Str1 : " + str1);
+		
+		String str2 = "20" + (2+8);
+		System.out.println("Str2 : " + str2);
+		
+		String str3 = 20 + "2"+8;
+		///System.out.println("Str3 : " + str3);*/
+		
+		//int value1 = Integer.parseInt("10");
+		//double value2 = Double.parseDouble("3.14");
+		//boolean value3 = Boolean.parseBoolean("true");
+		
+		//System.out.println("value1 : " + value1);
+		//System.out.println("value2 : " + value2);
+		//System.out.println("value3 : " + value3);
+
+		
+		
+		
+	    int x = 5;
+		int y = 2;
+		
+		int result = x/y;
+		
+		System.out.println(result);
+		
+		//int x = 5;
+		//int y = 2;
+		
+		//double result = (double)x/y;
+		
+		System.out.println(result);
+
+		
+		
+		//소숫점 날리는 코드
+	//	double var1 = 3.5;
+		//double var2 = 2.7;
+		
+		//double Value = (double) 3.5+2.7;
+		
+		//System.out.println("var : "+ (int)Value );
+		
+	
 		
 		
 	}
